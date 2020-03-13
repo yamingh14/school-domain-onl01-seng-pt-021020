3edd728d13a47557351a006695e120bbaaf67cf1
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-03-13 03:41:41 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class School
 
   def initialize(school_name)
@@ -9,22 +10,22 @@ class School
     @roster
   end
   #
-  # def add_student(name, grade)
-  #   @student_name = name
-  #   @grade = grade
-  #   if @roster.include?(grade) == false
-  #     @roster[grade] = []
-  #   end
-  #   @roster[grade] << name
-  # end
+  def add_student(name, grade)
+    @student_name = name
+    @grade = grade
+    if @roster.include?(grade) == false
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
   #
-  # def grade(number)
-  #   @roster[number]
-  # end
-  #
-  # def sort
-  #   @roster.each do |grade, name|
-  #     @roster[grade] = name.sort
-  #   end
-  # end
+  def grade(number)
+    @roster[number]
+  end
+
+  def sort
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
+    end
+  end
 end
